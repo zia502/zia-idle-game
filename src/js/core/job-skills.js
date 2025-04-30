016c093d20cfa4fc2158e41a593ad52e367b2e58
juchenjun <cjju@xreal.com>
2025-04-30 17:49:53 +0800

    fix damage calc

--- src/js/core/job-skills.js
+++ src/js/core/job-skills.js
@@ -392,7 +392,7 @@ const JobSkills = {
         }
 
         // 原始伤害是"造成伤害"
-        let finalDamage = rawDamage;
+        let finalDamage = rawDamage / 10;  // 将原始伤害除以10
         let isCritical = false;
         let attributeBonus = 0;
 
